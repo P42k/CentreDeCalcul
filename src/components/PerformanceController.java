21e6d6c1bf558069f1682e813593cfcbbeefe7a4
JuliaMW <jmaria.wisniewski@gmail.com>
2015-01-16 00:59:27 +0100

    comentaires

--- src/components/PerformanceController.java
+++ src/components/PerformanceController.java
@@ -4,5 +4,8 @@ import fr.upmc.components.AbstractComponent;
 /** composant simulant le contrôleur de performance */
 
 public class PerformanceController extends AbstractComponent {
-
+	//garder le tmps moyen d'une requête
+	//changer la fréquence si au-dessus/en dessous d'un certain seuil, changer la fréquence ou sinon la même mais avec la taille de la
+	//file d'attente.
+	//Faire des threads comme dans le controller
 }
