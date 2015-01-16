2dfc7a0b13e5a168d96bb1e60e577c02073e7b67
JuliaMW <jmaria.wisniewski@gmail.com>
2015-01-16 08:54:35 +0100

    petit truc

--- src/components/PerformanceController.java
+++ src/components/PerformanceController.java
@@ -8,4 +8,17 @@ public class PerformanceController extends AbstractComponent {
 	//changer la fréquence si au-dessus/en dessous d'un certain seuil, changer la fréquence ou sinon la même mais avec la taille de la
 	//file d'attente.
 	//Faire des threads comme dans le controller
+	/** Temps moyen d'une requête */
+	private long meanTime;
+	/** Seuil à partir duquel il faut faire descendre la fréquence */
+	static final long lowThreshold=10000L; // à définir
+	
+	/**Seuil à partir duquel il faut augmenter la fréquence des coeurs */
+	static final long highThreshold=100000L;
+	
+	/** crée un contrôleur de performances */
+	public PerformanceController(){
+		
+	}
+	
 }
